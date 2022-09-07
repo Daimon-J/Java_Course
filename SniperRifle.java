7c6a0079522db825a554552465731c302a4de56d
Daimon <89958797+Daimon-J@users.noreply.github.com>
2022-09-07 13:29:08 +0300

    Add files via upload

---
+++ SniperRifle.java
@@ -0,0 +1,21 @@
+public class SniperRifle {
+    public static void main(String[] args) {
+
+        byte weight = 10;
+
+        double price = 1.998d;
+        float caliber = 6.5f;
+        float total = 44.5f;
+
+        short barrel = 24;
+
+        char signOfDollar = '$';
+
+        System.out.println("1 Tikka T3x Tac A1");
+        System.out.println("Price: " + signOfDollar + price);
+        System.out.println("Caliber: " + caliber);
+        System.out.println("Barrel: " + barrel + " inch");
+        System.out.println("Total: " + total + " inch");
+        System.out.println("Weight: " + weight + "lb");
+    }
+}
